c7c408237d6d02edf2a4f6a8aa0ae6629f8f35ee
ChuanYinSia <67416563+ChuanYinSia@users.noreply.github.com>
2020-06-26 10:56:48 +0800

    Update dnsrecord.py

--- dnsrecord.py
+++ dnsrecord.py
@@ -8,18 +8,19 @@ from tqdm import tqdm
 
 def go(url):
     result = []
-    ids = ['A', 'NS', 'SOA', 'MX', 'TXT']
+    ids = ['NONE', 'A', 'NS', 'MD', 'MF', 'CNAME', 'SOA', 'MB', 'MG', 'MR', 'NULL', 'WKS', 'PTR', 'HINFO', 'MINFO', 'MX', 'TXT', 'RP', 'AFSDB', 'X25', 'ISDN', 'RT', 'NSAP', 'NSAP-PTR', 'SIG', 'KEY', 'PX', 'GPOS', 'AAAA', 'LOC', 'NXT', 'SRV', 'NAPTR', 'KX', 'CERT', 'A6',
+           'DNAME', 'OPT', 'APL', 'DS', 'SSHFP', 'IPSECKEY', 'RRSIG', 'NSEC', 'DNSKEY', 'DHCID', 'NSEC3', 'NSEC3PARAM', 'TLSA', 'HIP', 'CDS', 'CDNSKEY', 'CSYNC', 'SPF', 'UNSPEC', 'EUI48', 'EUI64', 'TKEY', 'TSIG', 'IXFR', 'AXFR', 'MAILB', 'MAILA', 'ANY', 'URI', 'CAA', 'TA', 'DLV']
     for a in ids:
         try:
             answers = dns.resolver.query(url, a)
-            for rdata in answers:
-                bb = a + ':' + rdata.to_text()
+            for ans in answers:
+                bb = a + ':' + ans.to_text()
                 result.append(bb)
         except:
-            result = []
+            pass
 
     refinal = json.dumps(result)
-    final = refinal.replace("\'", "")
+    final = refinal.replace("\\", "")
     print(url, final, '\n')
 
 
